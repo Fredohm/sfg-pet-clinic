d309a0cee63a108d7e91d3a10467d13575bf1dd5
Fredohm <fredwaucquez@hotmail.fr>
2020-11-04 09:51:41 +0100

    Loaded Visits on Startup on Bootstrap class. closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -26,7 +26,7 @@ public class VisitMapService extends AbstractMapService<Visit,Long> implements V
 
         if (visit.getPet() == null || visit.getPet().getOwner() == null
                 || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
-            throw new RuntimeException("Invialid visit");
+            throw new RuntimeException("Invalid visit");
         }
 
         return super.save(visit);
